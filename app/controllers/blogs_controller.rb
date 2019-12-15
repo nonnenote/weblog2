f4b5e3e6ce290dada2bfefea29ebc2be3e0513a4
nozomi <kawasaki070@yahoo.co.jp>
2019-12-16 05:06:03 +0900

    ログイン機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,10 +1,11 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :login_require, only: [:new, :edit, :show, :destroy]
 
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     if params[:back]
       @blog = Blog.new(blog_params)
@@ -12,7 +13,7 @@ class BlogsController < ApplicationController
       @blog = Blog.new
     end
   end
-  
+
   def create
     @blog = Blog.new(blog_params)
     if @blog.save
@@ -21,15 +22,15 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def show
-  
+
   end
-  
+
   def edit
-    
+
   end
-  
+
   def update
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
@@ -37,25 +38,29 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
   end
-  
+
   def confirm
     @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
+
   private
 
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   def set_blog
     @blog = Blog.find(params[:id])
   end
-  
+
+  def login_require
+    flash[:notice] = 'ログインしてください'
+    redirect_to blogs_path unless current_user
+  end
 end
